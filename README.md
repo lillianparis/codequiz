353c75b6f7377d77eb2dcf69d3128741d2bd128f
Lillian Paris <lillian.paris529@gmail.com>
2020-09-03 09:09:00 -0700

    added pictures to the readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # codequiz
 Week 4, Working with APIs
+<img width="1261" alt="Screen Shot 2020-09-03 at 8 59 13 AM" src="https://user-images.githubusercontent.com/62733242/92139503-ab356e80-edc4-11ea-89af-e971c77454f3.png">
+
+<img width="1255" alt="Screen Shot 2020-09-03 at 8 59 45 AM" src="https://user-images.githubusercontent.com/62733242/92139512-acff3200-edc4-11ea-89e8-1afe300b21be.png">
+
+<img width="1227" alt="Screen Shot 2020-09-03 at 9 00 14 AM" src="https://user-images.githubusercontent.com/62733242/92139515-affa2280-edc4-11ea-9d40-eb96de625385.png">
+
 This webpage is going to feature a multiple-choice question quiz, with interactive
 challenges. This page will be able to adapt to multiple screen sizes to broaden its
 interactivity to a wider audicence than just pc and desktop! This quiz is going to
--- quiz.js
+++ quiz.js
@@ -3,140 +3,148 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const button4 = document.querySelector("#button4");
-const timEl = document.querySelector(".time");
+const timeEl = document.querySelector(".time");
 
 // Countdown timer, starts at 60 seconds
 let secondsLeft = 60;
 function setTime() {
-    let timerInterval = setInterval(function () {
-        secondsLeft--;
-        timerInterval.textContent = "Time Left: " + secondsLeft;
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-        }
-    }, 1000);
+  let timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time Left: " + secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
 }
 
 setTime();
 
 // Questions and answers, need to fix the capital letters (camel case is prefered)
 let questionAnswer = [
-    {
-        Question: "Choose the client-side Javascript object",
-        Answers: ["Database",
-            "Cursor",
-            "HClient",
-            "FileUpLoad"
+  {
+    Question: "Choose the client-side Javascript object",
+    Answers: ["Database", "Cursor", "HClient", "FileUpLoad"],
+    CorrectAnswer: "FileUpLoad",
+  },
+  {
+    Question: "What are varibles used for in JavaScript programs?",
+    Answers: [
+      "Storing numbers, dates, or other values",
+      "Varying randomly",
+      "Causing high-school algebra flashbacks",
+      "None of the above",
     ],
-        CorrectAnswer: "FileUpLoad"
-    }, 
-    {
-        Question: "What are varibles used for in JavaScript programs?",
-        Answers: ["Storing numbers, dates, or other values",
-        "Varying randomly",
-        "Causing high-school algebra flashbacks",
-        "None of the above"
-    ],
-        CorrectAnswer: "Storing numbers, dates, or other values"
-    },
+    CorrectAnswer: "Storing numbers, dates, or other values",
+  },
 
-    {
-        Question: "Which of the following are capabilities of functions in JavaScript?",
-        Answers: ["Return a value",
-        "Accept parameters and return a value",
-        "Accept parameters",
-        "None of the above"
+  {
+    Question:
+      "Which of the following are capabilities of functions in JavaScript?",
+    Answers: [
+      "Return a value",
+      "Accept parameters and return a value",
+      "Accept parameters",
+      "None of the above",
     ],
-        CorrectAnswer: "Accept parameters"
-    },
-    {
-        Question: "Inside which HTML element do we put the JavaScript?",
-        Answers: ["<js>",
-        "<scripting>",
-        "<script>",
-        "<javascript>"
+    CorrectAnswer: "Accept parameters",
+  },
+  {
+    Question: "Inside which HTML element do we put the JavaScript?",
+    Answers: ["<js>", "<scripting>", "<script>", "<javascript>"],
+    CorrectAnswer: "<javascript>",
+  },
+  {
+    Question: "What does the <noscript> tag do?",
+    Answers: [
+      "Enclose text to be displayed by non-JavaScript browsers",
+      "Prevents scripts on the page from executing",
+      "Describes certain low-budget movies",
+      "None of the above",
     ],
-        CorrectAnswer: "<javascript>"
-    },
-    {
-        Question: "What does the <noscript> tag do?",
-        Answers: ["Enclose text to be displayed by non-JavaScript browsers",
-        "Prevents scripts on the page from executing",
-        "Describes certain low-budget movies",
-        "None of the above"
-    ],
-        CorrectAnswer: "Enclose text to be displayed by non-JavaScript browsers"
-    }
-]
+    CorrectAnswer: "Enclose text to be displayed by non-JavaScript browsers",
+  },
+];
 
-
-
+// We start at 0
 let index = 0;
-let currentQuestion = questionAnswer[0]
+let currentQuestion = questionAnswer[0];
 
 nextQuestion();
 
 // Wrap code in a function
 function nextQuestion() {
-    currentQuestion = questionAnswer[index]
-    question.textContent = currentQuestion.Question;
-    button1.textContent = currentQuestion.Answers[0];
-    button2.textContent = currentQuestion.Answers[1];
-    button3.textContent = currentQuestion.Answers[2];
-    button4.textContent = currentQuestion.Answers[3];
-
+  currentQuestion = questionAnswer[index];
+  question.textContent = currentQuestion.Question;
+  button1.textContent = currentQuestion.Answers[0];
+  button2.textContent = currentQuestion.Answers[1];
+  button3.textContent = currentQuestion.Answers[2];
+  button4.textContent = currentQuestion.Answers[3];
 }
 
-    // Checking for correct answer from each button
+// "this" was added to ask whic button called the function.
 
-    button1.addEventListener("click", function(event) {
+for(let i = 1; i < 5; i++) {
+    document.querySelector("#button" + i).addEventListener("click", function (event) {
         event.preventDefault();
-        if (button1.textContent === currentQuestion.CorrectAnswer) {
-
-        alert("correct");
-    } 
-        else {
-            alert ("wrong")
+        if (this.textContent === currentQuestion.CorrectAnswer) {
+            alert("correct");
+        } else {
+            alert("wrong");
+            secondsLeft = secondsLeft - 10;
         }
         index++;
         nextQuestion();
     });
+    console.log(i)
+}
 
-    button2.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (button1.textContent === currentQuestion.CorrectAnswer) {
+// Checking for correct answer from each button
 
-        alert("correct");
-    } 
-        else {
-            alert ("wrong")
-        }
-        index++;
-        nextQuestion();
-    });
 
-    button3.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (button1.textContent === currentQuestion.CorrectAnswer) {
+// button1.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   if (button1.textContent === currentQuestion.CorrectAnswer) {
+//     alert("correct");
+//   } else {
+//     alert("wrong");
+//     secondsLeft = secondsLeft - 10;
+//   }
+//   index++;
+//   nextQuestion();
+// });
 
-        alert("correct");
-    } 
-        else {
-            alert ("wrong")
-        }
-        index++;
-        nextQuestion();
-    });
+// button2.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   if (button2.textContent === currentQuestion.CorrectAnswer) {
+//     alert("correct");
+//   } else {
+//     alert("wrong");
+//     secondsLeft = secondsLeft - 10;
+//   }
+//   index++;
+//   nextQuestion();
+// });
 
-    button4.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (button1.textContent === currentQuestion.CorrectAnswer) {
+// button3.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   if (button3.textContent === currentQuestion.CorrectAnswer) {
+//     alert("correct");
+//   } else {
+//     alert("wrong");
+//     secondsLeft = secondsLeft - 10;
+//   }
+//   index++;
+//   nextQuestion();
+// });
 
-        alert("correct");
-    } 
-        else {
-            alert ("wrong")
-        }
-        index++;
-        nextQuestion();
-    });
+// button4.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   if (button4.textContent === currentQuestion.CorrectAnswer) {
+//     alert("correct");
+//   } else {
+//     alert("wrong");
+//     secondsLeft = secondsLeft - 10;
+//   }
+//   index++;
+//   nextQuestion();
+// });
